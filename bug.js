3b0d193b311f4936685f90e71ad833b8df357e41
BugHunter-X <askzenai@gmail.com>
2025-02-09 18:58:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// Some Tailwind CSS classes are not applied to the element.
+<div class="bg-red-500 hover:bg-blue-700 p-4">
+  This div should have a red background and change to blue on hover.
+</div>
+```
